0b6302f29a050c78f278e16b6f67ba8fb97b78a2
Gordon Yip <zcapkhy@ucl.ac.uk>
2020-08-15 13:50:48 +0100

    made sure spectrum is in 2d format

--- models/utils.py
+++ models/utils.py
@@ -25,6 +25,8 @@ def normalise(array, arr_max, arr_min):
 
 
 def normalise_spectrum(spectrum):
+    if spectrum.ndim == 1:
+        spectrum = spectrum.reshape(-1, len(spectrum))
     local_min = spectrum.min(axis=1, keepdims=True)
     local_max = spectrum.max(axis=1, keepdims=True)
     norm_spectrum, _, _ = normalise(spectrum, local_max, local_min)
